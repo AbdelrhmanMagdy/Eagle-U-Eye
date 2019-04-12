1c9e241acbcef17863469f11bd88975b93117fe3
Nader Yasser <nader.yasser.93@gmail.com>
2019-04-12 03:47:25 +0200

    STATIC : list of trackers && streams

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 import './Container.css'
+import Stream from './Stream';
+
  class Container extends Component {
   render() {
     return (
       <div className='container'>
-        Container
+        <div className='streams'>
+          <Stream src ={require('../../images/1.png')} class = 'stream' />
+          <Stream src ={require('../../images/2.png')}  class = 'stream' />
+          <Stream src ={require('../../images/3.png')}  class = 'stream' />
+          <Stream src ={require('../../images/4.png')}  class = 'stream' />
+
+        </div>
       </div>
     )
   }
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -4,7 +4,15 @@ import './Sidebar.css';
  const Sidebar =()=> {
   return (
     <div className='side-bar'>
-      List
+    <h3>List of Trackers</h3>
+      <div className='person'> person 1</div>
+      <div className='person'> person 2</div>
+      <div className='person'> person 3</div>
+      <div className='person'> person 4</div>
+      <div className='person'> person 5</div>
+      <div className='person'> person 6</div>
+
+
     </div>
   )
 }
